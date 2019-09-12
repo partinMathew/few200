1a04f5d356b33688b07afe5a565266021d6e45f5
partinMathew <partinmathew@gmail.com>
2019-09-12 11:58:09 -0400

    added entry component

--- src/app/features/books/components/entry/entry.component.ts
+++ src/app/features/books/components/entry/entry.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BooksState } from '../../reducers';
+import { bookAdded } from '../../actions/list.actions';
 
 @Component({
   selector: 'app-entry',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entry.component.css']
 })
 export class EntryComponent implements OnInit {
+  constructor(private store: Store<BooksState>) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addBook(
+    titleEl: HTMLInputElement,
+    authorEl: HTMLInputElement,
+    formatEl: HTMLInputElement
+  ) {
+    this.store.dispatch(
+      bookAdded({
+        title: titleEl.value,
+        author: authorEl.value,
+        format: formatEl.value
+      })
+    );
+    titleEl.value = '';
+    authorEl.value = '';
+    formatEl.value = '';
+    titleEl.focus();
   }
-
 }
